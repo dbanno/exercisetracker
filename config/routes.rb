614c83799f33e0da42f57c5b42689f4362b2ec5a
dan <dbanno@gmail.com>
2014-07-05 18:53:13 -0700

    Add Signout and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :users
 
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
-  match '/help',				to:	'static_pages#help',		via: 		'get'
-  match '/signup',			to:	'users#new',					via:		'get'
+  match '/help',				to: 'static_pages#help',			via:	'get'
+  match '/signup',			to: 'users#new',					via:	'get'
+  match '/signin',  			to: 'sessions#new',         		via:	'get'
+  match '/signout', 		to: 'sessions#destroy',     	via: 	'delete'
   resources :exercises
   resources :weight_logs
   
