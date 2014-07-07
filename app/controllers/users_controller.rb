674f83dae1394d2fd2ab31154a4d0e005d46176e
dan <dbanno@gmail.com>
2014-07-06 20:09:22 -0700

    Fix user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    set_user
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
