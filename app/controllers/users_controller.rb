8b2e42d549a2df3f0a598df0d1474723634b69f1
dan <dbanno@gmail.com>
2014-07-08 22:33:14 -0700

    Add last logged in. Change weight_log form to be formatted for numbers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
 	set_user
+	#@weight_logs = WeightLog.joins(exercise: :user).sum("weight")
   end
 
   # GET /users/new
