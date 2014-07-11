dd16b91d66121f1efe61983a7257ef40975d6922
dan <dbanno@gmail.com>
2014-07-10 21:10:17 -0700

    Removed } from Production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,6 @@ Rails.application.configure do
 	  :enable_starttls_auto => true
 }
   
-  }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
