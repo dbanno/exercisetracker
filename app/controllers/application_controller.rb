942f77fe4bfdf45951caaffd1be66b958587a948
dan <dbanno@gmail.com>
2014-07-11 20:24:55 -0700

    Add timezone to User and convert dates to users timezone.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :update_sanitized_params, if: :devise_controller?
   protect_from_forgery with: :exception
 	#include SessionsHelper
-  
+	
+	protected
+		  def update_sanitized_params
+			devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name,:email,:password_confirmation, :current_password,:password,:timezone)}
+		  end
 end
---
+++ db/migrate/20140712030026_add_time_zone_to_users.rb
@@ -0,0 +1,5 @@
+class AddTimeZoneToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :timezone, :string, :limit => 255, :default => "UTC"
+  end
+end
